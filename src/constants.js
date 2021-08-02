d9a453640d9d0ffdba76a2ab9be6a296ba15ba1d
Aman Jain <aman.jain@enrichai.com>
2021-08-02 08:24:45 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+	BASE_URL: "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=469468127506a1e76dd33836f37e0ceb&per_page=20&format=json&nojsoncallback=1",
+	GET_RECENT_URL: "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=469468127506a1e76dd33836f37e0ceb&extras=recent&per_page=20&page=1&format=json&nojsoncallback=1",
+	STORAGE_KEY: "flicker_search_queries"
+};
+
+export default constants;
